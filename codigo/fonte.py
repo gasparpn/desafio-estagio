4cf22a70896b84251f301729032f91018b6567de
Gaspar Leite Pereira Neto <gasparneto95@gmail.com>
2019-02-16 18:15:19 -0200

    Modifica código para não salvar os cadernos

--- codigo/fonte.py
+++ codigo/fonte.py
@@ -2,7 +2,6 @@ import urllib
 import requests
 import re
 import hashlib
-import os
 
 
 def BuscaIdDiarios(data):
@@ -28,9 +27,9 @@ def BuscaIdDiarios(data):
     return regexBuscaIdDiario
 
 
-def FazDownloadDiarios(listaDiariosId, dataDiarios):
+def FazHashDiarios(listaDiariosId, dataDiarios):
 
-    listaNome = []
+    md5Hash = hashlib.md5()
     for index, diarioId in enumerate(listaDiariosId):
         parametrosUrlBaixaDiario = {
             'action': 'downloadDiario',
@@ -40,43 +39,17 @@ def FazDownloadDiarios(listaDiariosId, dataDiarios):
             'id': diarioId, 'tribunal': 'TSE'}
 
         parametrosUrlBaixaDiario = urllib.parse.urlencode(parametrosUrlBaixaDiario)
-        respostaUrlBaixaDiario = requests.get('http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do', params=parametrosUrlBaixaDiario)
+        urlBaixaDiarios = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do'
+        respostaUrlBaixaDiario = requests.get(urlBaixaDiarios, params=parametrosUrlBaixaDiario)
+
         nomeDiario = dataDiarios.replace('/', '-')
-        if index == 0:
-            nomePasta = '../cadernos/{}'.format(nomeDiario)
-            listaNome.append(nomePasta)
-            if os.path.isdir('../cadernos/{}'.format(nomePasta)) is True:
-                print("Já existe uma pasta para essa data, para tentar novamente a apague: {}".format(nomePasta))
-                SystemExit
-            else:
-                os.mkdir(nomePasta)
 
-        nomeDiario = '{}-{}'.format(nomeDiario, index+1)
-        listaNome.append(nomeDiario)
-        with open('{}/{}'.format(nomePasta, nomeDiario), 'wb') as arquivo:
+        with open('../cadernos/{}'.format(nomeDiario), 'a+') as arquivo:
             for parte in respostaUrlBaixaDiario.iter_content(chunk_size=1024):
-                arquivo.write(parte)
-        arquivo.close()
-    return listaNome
-
-
-def FazHashDiarios(listaNome):
-    md5Hash = hashlib.md5()
-    for index, nomeDiario in enumerate(listaNome):
-        if index == len(listaNome) - 1:
-            break
-        with open('{}/{}'.format(listaNome[0], listaNome[index+1]), 'rb') as arquivo:
-            while True:
-                parte = arquivo.read(1024)
-                if parte == b'':
-                    break
-                else:
-                    md5Hash.update(parte)
-        with open('{}-hash'.format(listaNome[0], listaNome[0]), 'a+') as arquivo:
+                md5Hash.update(parte)
             hexadecimalHash = md5Hash.hexdigest()
             arquivo.write(hexadecimalHash)
             arquivo.write('\n')
-        arquivo.close()
 
 
 dataDiarios = input("Entre com uma data no formato: dd/mm/aaaa: ")
@@ -85,9 +58,4 @@ if not listaIdDiarios:
     print("Nenhum diário foi encontrado para a data {}".format(dataDiarios))
     SystemExit
 else:
-    listaNome = FazDownloadDiarios(listaIdDiarios, dataDiarios)
-    if not listaNome:
-        print("Falha ao tentar fazer downloads dos diários para a data {}".format(dataDiarios))
-        SystemExit
-    else:
-        FazHashDiarios(listaNome)
+    FazHashDiarios(listaIdDiarios, dataDiarios)
